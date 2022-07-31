bd4133f105d90348c7bd4a1ac185e9afd93d5ce0
Febin Mubarak <febinmubarak333@gmail.com>
2022-07-31 19:11:52 +0400

    Design completed

--- landingpage/src/Components/Footer/Footer.jsx
+++ landingpage/src/Components/Footer/Footer.jsx
@@ -7,24 +7,24 @@ import Fade from 'react-reveal/Fade';
 
 function Footer() {
   return (
-    <div className='footer'>
+    <div  className='footer'>
         <div className='footerheading'>
-         <Fade bottom duration={1500}><Card 
+         <Fade bottom duration={3500}><Card 
              heading={"There are many variations of passages of Lorem Ipsum"}
              details={"but the majority have suffered alteration in some form"}
              details2={"randomised words which don't look"}
          /></Fade>
          </div>
          <div className="footerheading2">
-         <Fade bottom duration={1600}><Card
+         <Fade bottom duration={4500}><Card
                heading={"Richard McClintock, a Latin professor at Hampden "}
-               details={" All the Lorem Ipsum generators on the Internet tend "}
+               details={"All the Lorem Ipsum generators on the Internet tend "}
                details2={"to repeat predefined chunks as necessary"}
             
             /></Fade>
          </div>
-         <Fade left duration={2000}> <img  className='img3' src={home} alt="" /></Fade>
-        <Fade right duration={2000}> <img className='img4' src={home2} alt="" /></Fade>
+         <Fade left duration={2500}> <img  className='img3' src={home} alt="" /></Fade>
+        <Fade right duration={2500}> <img className='img4' src={home2} alt="" /></Fade>
     </div>
   )
 }
--- landingpage/src/Components/Home/Home.jsx
+++ landingpage/src/Components/Home/Home.jsx
@@ -3,6 +3,8 @@ import "./Home.css"
 import Card from '../Card/Card';
 import burj from "../../img/burj.png"
 import Fade from 'react-reveal/Fade';
+import {Link} from "react-scroll"
+
 
 
 
@@ -14,23 +16,25 @@ function Home() {
        
         
         <Fade bottom duration={1500}>
-        <p className='heading1'>Lorem Ipsum is simply dummy printing and <br/> text of the printing and typesetting industry remaining essentially</p>
+        <p className='heading1'>Lorem Ipsum is simply dummy printing and <br/> text of the printing and typesetting industry </p>
         </Fade>
-   <Fade left duration={2300}> <Card 
+   <Fade left duration={2500}> <Card 
     
-         heading={"The point of using Lorem Ipsum is that it has a more"}
+         heading={"The point of using Lorem Ipsum is that it"}
          details={"Many desktop publishing packages and web page editors now use "}
-         details2={"Lorem Ipsum  as their default model text, and a search for 'lorem ipsum' will uncover many web sites"}
+         details2={"Lorem Ipsum  as their default model text, and a search for 'lorem ipsum' will uncover many"}
     /></Fade>
-    <Fade left duration={3000} ><img height="420" width="320" className='img1' src={burj} alt="" /></Fade>
+    <Fade left duration={3500} ><img  className='img1' src={burj} alt="" /></Fade>
+    <Link spy={true} to='footer' smooth={true} activeclass="activeclass"  >
+    <Fade bottom duration={1500}><button className='button button1'>scroll</button></Fade>
+                    </Link>
     
-    <Fade bottom duration={1500}><button className='button button1'>to scroll</button></Fade>
     
    <div style={{top:"23rem",position:"absolute"}}
-     ><Fade left duration={2300} ><Card 
+     ><Fade left duration={2500} ><Card 
       heading={"Lorem Ipsum is not simply random text"}
-        details={"It has roots in a piece of classical Latin literature from 45 BC"}
-        details2={"Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of"}
+        details={"It has roots in a piece of classical Latin literature from 45 BC ne of"}
+        details2={"Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up o"}
     /></Fade ></div>
         
     
