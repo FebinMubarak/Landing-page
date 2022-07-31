7ac5eb6bb0b59997d761d54d6c31d38945bae3ad
Febin Mubarak <febinmubarak333@gmail.com>
2022-07-31 17:22:11 +0400

    dynamically revealed text and images

--- landingpage/src/Components/Footer/Footer.jsx
+++ landingpage/src/Components/Footer/Footer.jsx
@@ -3,27 +3,28 @@ import Card from "../Card/Card"
 import "./Footer.css"
 import home from "../../img/home.png"
 import home2 from "../../img/home2.png"
+import Fade from 'react-reveal/Fade';
 
 function Footer() {
   return (
     <div className='footer'>
         <div className='footerheading'>
-         <Card 
+         <Fade bottom duration={1500}><Card 
              heading={"There are many variations of passages of Lorem Ipsum"}
              details={"but the majority have suffered alteration in some form"}
              details2={"randomised words which don't look"}
-         />
+         /></Fade>
          </div>
          <div className="footerheading2">
-            <Card
+         <Fade bottom duration={1600}><Card
                heading={"Richard McClintock, a Latin professor at Hampden "}
                details={" All the Lorem Ipsum generators on the Internet tend "}
                details2={"to repeat predefined chunks as necessary"}
             
-            />
+            /></Fade>
          </div>
-         <img className='img3' src={home} alt="" />
-         <img className='img4' src={home2} alt="" />
+         <Fade left duration={2000}> <img  className='img3' src={home} alt="" /></Fade>
+        <Fade right duration={2000}> <img className='img4' src={home2} alt="" /></Fade>
     </div>
   )
 }
--- landingpage/src/Components/Home/Home.jsx
+++ landingpage/src/Components/Home/Home.jsx
@@ -2,29 +2,38 @@ import React from 'react'
 import "./Home.css"
 import Card from '../Card/Card';
 import burj from "../../img/burj.png"
-import burj2 from "../../img/burj2.png"
+import Fade from 'react-reveal/Fade';
+
+
 
 function Home() {
   return (
     <div className="homepage">
         <div className="container1">
-    <div className='heading1'>Lorem Ipsum is simply dummy printing and <br/> text of the printing and typesetting industry remaining essentially</div>
-
-    <Card 
+        
+       
+        
+        <Fade bottom duration={1500}>
+        <p className='heading1'>Lorem Ipsum is simply dummy printing and <br/> text of the printing and typesetting industry remaining essentially</p>
+        </Fade>
+   <Fade left duration={2300}> <Card 
     
          heading={"The point of using Lorem Ipsum is that it has a more"}
          details={"Many desktop publishing packages and web page editors now use "}
          details2={"Lorem Ipsum  as their default model text, and a search for 'lorem ipsum' will uncover many web sites"}
-    />
-    <img className='img1' src={burj} alt="" />
+    /></Fade>
+    <Fade left duration={3000} ><img height="420" width="320" className='img1' src={burj} alt="" /></Fade>
     
-    <button className='button button1'>Contact us</button>
-    <div style={{top:"29rem",position:"absolute"}}
-     ><Card 
-        heading={"Lorem Ipsum is not simply random text"}
+    <Fade bottom duration={1500}><button className='button button1'>to scroll</button></Fade>
+    
+   <div style={{top:"23rem",position:"absolute"}}
+     ><Fade left duration={2300} ><Card 
+      heading={"Lorem Ipsum is not simply random text"}
         details={"It has roots in a piece of classical Latin literature from 45 BC"}
         details2={"Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of"}
-    /> </div>
+    /></Fade ></div>
+        
+    
     </div>
     </div>
   )
